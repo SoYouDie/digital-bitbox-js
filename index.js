869c375bcb05ed36963ac1f47ef4720028e8dca2
SoYouDie <roman.b@essentia.one>
2018-04-28 14:24:23 +0300

    Fix requirements

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var digitalBitboxUsb = require('digitalBitboxUsb/digitalBitboxUsb')
-var digitalBitboxEth = require('digitalBitboxEth/digitalBitboxEth')
+var digitalBitboxUsb = require('./digitalBitboxUsb/digitalBitboxUsb')
+var digitalBitboxEth = require('./digitalBitboxEth/digitalBitboxEth')
 
 module.exports.digitalBitboxUsb = digitalBitboxUsb
 module.exports.digitalBitboxEth = digitalBitboxEth
