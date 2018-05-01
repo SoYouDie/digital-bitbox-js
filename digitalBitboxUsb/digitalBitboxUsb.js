b2cff2f867f371f363e4c71af77744dffa13ef76
SoYouDie <roman.b@essentia.one>
2018-05-01 12:35:14 +0300

    Add static u2f api

--- digitalBitboxUsb/digitalBitboxUsb.js
+++ digitalBitboxUsb/digitalBitboxUsb.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-var u2f = require('u2f-api');
+var u2f = require('./../u2f/u2f-api');
 
 var DigitalBitboxUsb = function(timeoutSeconds) {
     this.timeoutSeconds = timeoutSeconds;
